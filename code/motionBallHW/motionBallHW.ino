84b8b1dc23cdfecad12d42c1a6e02063d3aede6c
agcoda <agcoda@gmail.com>
2023-07-18 15:26:42 -0700

    organization

--- code/motionBallHW/motionBallHW.ino
+++ code/motionBallHW/motionBallHW.ino
@@ -1,23 +1,24 @@
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
 
+//Have to use Serial1 for the pro micro, switch to //! if debugging with regular ard
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
 double A[3] = {0.00,0.00,0.00};
 //char BTRun = "0";
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  //!Serial.begin(9600);
+  Serial1.begin(9600);
   //start the mma and set range to 2g. 4g and 8g also available for higher prcision
   mma.begin();
   mma.setRange(MMA8451_RANGE_2_G);
 }
 
 void loop() {
-  
-  
-  Serial.read();
+    
+  //!Serial.read();
+  Serial1.read();
 
   //reads the raw data from mma
   mma.read();
@@ -28,10 +29,11 @@ void loop() {
   A[0] = event.acceleration.x;
   A[1] = event.acceleration.y;
   A[2] = event.acceleration.z;
-  /*if(Serial.available()){
-  BTRun = Serial.read();
+  /*if(Serial1.available()){
+  BTRun = Serial1.read();
   }*/
 
+/* //!
 //   if(BTRun == "1"){
   Serial.print("{");
   Serial.print(A[0]);
@@ -42,5 +44,17 @@ void loop() {
   // }
    //else if(BTRun =="0"){
     Serial.read();
+    */
+
+//   if(BTRun == "1"){
+  Serial1.print("{");
+  Serial1.print(A[0]);
+  Serial1.print(",");
+  Serial1.print(A[1]);
+  Serial1.print(",");
+  Serial1.print(A[2]);
+  // }
+   //else if(BTRun =="0"){
+    Serial1.read();
   delay(200);
 }
